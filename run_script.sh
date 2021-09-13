99a59dcf5faca6467e1bb9770ead9bcd962c1504
Joseph Tafese <jetafese@lilla-my.cs.uwaterloo.ca>
2021-09-13 19:21:09 -0400

    generate partial AST and MLIR dialect for btor2example

---
+++ run_script.sh
@@ -0,0 +1,10 @@
+cd /home/jetafese/llvm-project/build
+cmake -G Ninja ../llvm \
+   -DLLVM_ENABLE_PROJECTS=mlir \
+   -DLLVM_BUILD_EXAMPLES=ON \
+   -DLLVM_TARGETS_TO_BUILD="X86;NVPTX;AMDGPU" \
+   -DCMAKE_BUILD_TYPE=Release \
+   -DLLVM_ENABLE_ASSERTIONS=ON
+cmake --build . --target check-mlir
+cd /home/jetafese/llvm-project/mlir/examples/btor2mlir
+

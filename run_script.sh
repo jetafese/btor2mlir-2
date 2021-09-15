6a98f23870aa9909110205117c26fba7b77f9f23
Joseph Tafese <jetafese@lilla-my.cs.uwaterloo.ca>
2021-09-15 15:49:00 -0400

    generate debug build instead of release build

--- run_script.sh
+++ run_script.sh
@@ -3,7 +3,7 @@ cmake -G Ninja ../llvm \
    -DLLVM_ENABLE_PROJECTS=mlir \
    -DLLVM_BUILD_EXAMPLES=ON \
    -DLLVM_TARGETS_TO_BUILD="X86;NVPTX;AMDGPU" \
-   -DCMAKE_BUILD_TYPE=Release \
+   -DCMAKE_BUILD_TYPE=Debug \
    -DLLVM_ENABLE_ASSERTIONS=ON
 cmake --build . --target check-mlir
 cd /home/jetafese/llvm-project/mlir/examples/btor2mlir
